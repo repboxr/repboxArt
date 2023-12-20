0b4ab7e9ceb0b3c153c78d14cd3ee72f94870cf4
skranz-auto <xsx50@uni-ulm.de>
2023-12-20 15:30:24 +0100

    Updates

--- R/art_options.R
+++ R/art_options.R
@@ -5,7 +5,7 @@
 #'
 #' If single_line_regs=FALSE we always assume that multiple rows correspond to single regression. If single_line_regs=TRUE we assume all tables show single line regression. If single line is a data frame it can have columns tabid and panel_num to specify which tables and possible panels are assumed to show single line regressions.
 #' @param not_implemented_action "error", "warn", "msg", or "nothing". The text analysis currently uses crude heuristics to detect elements, like sections, page headers and footers, and footnotes for PDF. Currently the heuristics are implemented just for a few journals (AEA journals). If a feature is not implemented for the article's journal shall an error be thrown, a warining or nothing be done.
-repbox_art_opts = function(journ_format = NULL, overwrite=FALSE,single_line_regs = FALSE, few_integer_as_text_limit = 5, not_implemented_action=c("error","warn","msg","nothing")[1]) {
+repbox_art_opts = function(journ_format = NULL, overwrite=FALSE,single_line_regs = FALSE, few_integer_as_text_limit = 5, not_implemented_action=c("error","warn","msg","nothing")[3]) {
   list(
     overwrite = overwrite,
     single_line_regs = single_line_regs,
